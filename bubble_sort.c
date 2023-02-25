2a1909789ce1f136f545cf125ae758a422cf5357
marianohtl <8.litah.8@gmail.com>
2023-02-25 13:45:42 -0300

    removing excessive rotates.

--- bubble_sort.c
+++ bubble_sort.c
@@ -1,24 +1,41 @@
 #include "stacks.h"
+#include <unistd.h>
+
+int	is_stack_sorted(t_stacks *stacks)
+{
+	int				index;
+	t_linked_list	*current;
+
+	if (stacks->stack_b != NULL)
+		return (0);
+	index = 0;
+	current = stacks->stack_a;
+	while (index < stacks->size_a - 1)
+	{
+		if (current->content > current->next->content)
+			return (0);
+		current = current->next;
+		index++;
+	}
+	return (1);
+}
 
 void	bubble_sort(t_stacks *stacks)
 {
 	int	is_sorted;
 	int	index;
 
-	is_sorted = 0;
+	is_sorted = is_stack_sorted(stacks);
 	while (is_sorted == 0)
 	{
 		index = 0;
-		is_sorted = 1;
 		while (index < stacks->size_a)
 		{
 			if (index < stacks->size_a - 1 && stacks->stack_a->content >= stacks->stack_a->next->content)
-			{
 				swap_a(stacks);
-				is_sorted = 0;
-			}
 			rotate_a(stacks);
 			index++;
 		}
+		is_sorted = is_stack_sorted(stacks);
 	}
 }
